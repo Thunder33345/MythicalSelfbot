222f7664eadc18ed54f57f4171a293d51e84fc60
HimbeersaftLP <mail@himbeer.me>
2017-08-06 00:00:39 +0200

    Delete things starting w/ [object after 5 secs when using eval

--- bot.js
+++ bot.js
@@ -33,16 +33,15 @@ bot.on('message', function(msg) {
       case 'eval':
         var code = args.join(' ');
         try {
-          let evaled = eval(code);
+          var evaled = eval(code);
           if (evaled === token || evaled == "token" || evaled.toString().indexOf(token) != -1 || evaled.toString().indexOf("token") != -1) {
             evaled = 'Don\'t eval your token, that is... Hmm, bad.';
             msg.delete();
           }
-          pro = msg.channel.send("\`\`\`\n" + evaled + "\n\`\`\`");
+          pro = msg.channel.send("\`\`\`\n" + evaled.toString() + "\n\`\`\`");
           // TODO: .catch handle message with over 2k chars
-          if (evaled !== null && typeof evaled === 'object') {
-            //TODO: check if evaled has custom toString, if it does let it live
-            pro.then(m => m.delete(60000))
+          if(S(evaled.toString()).startsWith('[object')){
+            pro.then(m => m.delete(5000));
           }
         } catch (err) {
           if (err !== null && typeof err === 'object') {
